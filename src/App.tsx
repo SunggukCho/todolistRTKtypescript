204e5d0980784ed7b41faf1ff56ec44c5617c072
SeanCho <sean@1011.co.kr>
2022-06-16 17:50:21 +0900

    fix: todoList useReducer 활용해서 refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, useReducer } from 'react';
 
 interface Todo {
   id: number;
@@ -7,11 +7,40 @@ interface Todo {
   done: boolean;
 }
 
+const ADD = "ADD";
+const DELETE = "DELETE";
+const TOGGLE = "TOGGLE";
+
+function reducer (state: any, action: any) {
+  switch (action.type) {
+    case ADD:
+      return state.concat(action.todo)
+    case DELETE:
+      return state.filter((todo: Todo) => todo.id !== action.todo.id)
+    case TOGGLE:
+      return action.todos
+    default:
+      return
+  }
+}
+
+const initialState: Todo[] = [
+  {
+    id: 1,
+    value: "Hello",
+    done: true
+  },
+  {
+    id: 2,
+    value: "World!",
+    done: false
+  }
+]
+
 function App() {
-  const todoId = useRef(0);
-  
+  const todoId = useRef(initialState.length);
   const [todo, setTodo] = useState("");
-  const [todoList, setTodoList] = useState<Todo[]>([]);
+  const [todos, dispatch] = useReducer(reducer, initialState);
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => setTodo(e.target.value);
   const handleSubmit = () => {
     todoId.current += 1;
@@ -20,18 +49,20 @@ function App() {
       value: todo,
       done: false
     }
-    setTodoList([...todoList, newTodo])
+    dispatch({ type: ADD, todo: newTodo })
+    setTodo("")
   }
-  const handleDelete = (deleteTodo: Todo) => setTodoList(todoList.filter((todo) => todo.id !== deleteTodo.id));
-  const toggle = (checkedTodo: Todo) => setTodoList(todoList.map(todo => todo.id === checkedTodo.id ? { ...todo, done: !todo.done } : todo));
-  // useEffect(()=>console.log(todoList))
+  const handleDelete = (deleteTodo: Todo) => dispatch({ type: "DELETE", todo: todos.filter((todo: Todo) => todo.id === deleteTodo.id)[0] });
+  const toggle = (checkedTodo: Todo) => dispatch({ type: TOGGLE, todos: todos.map((todo: Todo) => (todo.id === checkedTodo.id ? { ...todo, done: !todo.done } : todo)) });
+  
+  // useEffect(()=>console.log(todos))
 
   return (
     <div className="App">
       <label id="todo">Todo</label>
       <input type="text" id="todo" value={todo} onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleChange(e)} placeholder="입력" />
       <button onClick={handleSubmit}>+ADD</button>
-      {todoList.map((todo) => (
+      {todos.map((todo: Todo) => (
         <div key={todo.id}>
           <input type="checkbox" onChange={() => toggle(todo)} checked={todo.done} />
           {todo.id} {todo.value} <button onClick={()=>handleDelete(todo)}>-DEL</button>
